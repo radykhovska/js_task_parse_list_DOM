74aff5d6798c2ab75ed8b0b93f1b0a44775e23e8
radykhovska <zagnibeda@i.ua>
2026-04-21 16:32:12 +0300

    fix code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,13 +5,28 @@ const list = Array.from(document.querySelectorAll('li'));
 const parseToNumber = (string) => +string.replace(/[^0-9.-]+/g, '');
 
 const sortList = function (elements) {
-  return list.sort(
+  const sortedList = list.sort(
     (a, b) => parseToNumber(b.dataset.salary) - parseToNumber(a.dataset.salary),
   );
+
+  const ul = document.querySelector('ul');
+
+  sortedList.forEach((el) => ul.appendChild(el));
+};
+const getEmployees = (elements) => {
+  const rightObjects = [];
+
+  list.map((el) => {
+    rightObjects.push({
+      name: el.textContent.trim(),
+      position: el.dataset.position,
+      salary: el.dataset.salary,
+      age: el.dataset.age,
+    });
+  });
+
+  return rightObjects;
 };
 
-const sortedList = sortList(list);
-
-const ul = document.querySelector('ul');
-
-sortedList.forEach((el) => ul.appendChild(el));
+sortList(list);
+getEmployees(list);
