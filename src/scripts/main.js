a55d81a0f4cfc670257acccd620fdbea9b719b92
radykhovska <zagnibeda@i.ua>
2026-04-21 11:51:02 +0300

    sorted list

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write code here
+const list = Array.from(document.querySelectorAll('li'));
+
+const parseToNumber = (string) => +string.replace(/[^0-9.-]+/g, '');
+
+const sortList = function (elements) {
+  return list.sort(
+    (a, b) => parseToNumber(b.dataset.salary) - parseToNumber(a.dataset.salary),
+  );
+};
+
+const sortedList = sortList(list);
+
+const ul = document.querySelector('ul');
+
+sortedList.forEach((el) => ul.appendChild(el));
